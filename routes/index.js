fb9e23ec0670a6459cdc395be40722bb4c56c863
Roman Shestakov <neoroma@gmail.com>
2017-04-18 14:52:58 +0300

    Add handling some initial feeds and send parsed data to client

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,72 @@
-var express = require('express');
-var router = express.Router();
+import {Router} from 'express'
+import R from 'ramda'
+import https from 'https'
+import http from 'http'
+import RxNode from 'rx-node'
+import {Observable} from '@reactivex/rxjs'
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const router = Router()
 
-module.exports = router;
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'})
+})
+
+router.get('/btc', function (req, response, next) {
+
+    const baseParse = R.compose(
+        JSON.parse,
+        R.reduce(R.concat, ''),
+        R.map(R.toString))
+
+    const parseRate = R.compose(
+        R.pick(['USD', 'EUR']),
+        baseParse
+    )
+
+    const parseCoindesk = R.compose(
+        R.pick(['USD', 'EUR']),
+        R.prop('bpi'),
+        baseParse
+    )
+
+    const parseBitcoinchart = R.compose(
+        baseParse
+    )
+
+    const blockchain$ = observableFromRequest('https', 'https://blockchain.info/ticker', parseRate)
+    // const coindesk$ = observableFromRequest('http://api.coindesk.com/v1/bpi/currentprice.json', parseCoindesk)
+    // const bitcoinchart$ = observableFromRequest('http://api.bitcoincharts.com/v1/markets.json', parseBitcoinchart)
+
+    const pool = [blockchain$]
+
+    Observable.merge(...pool)
+        .bufferCount(pool.length)
+        .subscribe(data => {
+            response.json(data)
+        })
+
+})
+
+function observableFromRequest (protocol = 'http', feedUrl, parser) {
+
+    return Observable.create(subscriber => {
+
+        const client = protocol === 'http' ? http : https
+
+        client.get(feedUrl, (res) => {
+
+            RxNode.fromStream(res)
+                .bufferCount(10)
+                .subscribe(buffers => {
+                    subscriber.next(parser(buffers))
+                    subscriber.complete()
+                })
+
+        }).on('error', subscriber.error)
+
+        return () => {
+        }
+    })
+}
+
+export {router}
