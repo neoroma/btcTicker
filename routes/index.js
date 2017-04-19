e251600fcd4c7bb4b4caa53de668180d84863853
Roman Shestakov <neoroma@gmail.com>
2017-04-19 13:16:30 +0300

    Remove all `rest` endpoints except for stopping possibility, just for fun

--- routes/index.js
+++ routes/index.js
@@ -1,48 +1,11 @@
 import {Router} from 'express'
-import {Observable} from '@reactivex/rxjs'
-import {tick$, tearDown$} from '../ticker'
-import {observableFromFeed} from '../feedClient/client'
-
-import {packageTheResult} from '../feeds/util'
-
-import blockchainFeed from '../feeds/blockchain/blockchain.feed'
-import bitcoinchartsFeed from '../feeds/bitcoincharts/bitcoincharts.feed'
-import coindeskFeed from '../feeds/coindesk/coindesk.feed'
+import {tearDown$} from '../ticker'
 
 const router = Router()
 
-router.get('/tick', function (req, res, next) {
-    tick$.subscribe(val => console.log('Next tick: ', val))
-    res.status(200).send('ticker started')
-})
-
 router.get('/stop', function (req, res, next) {
     tearDown$.next()
-    res.status(200).send('ticker stopped')
-})
-
-router.get('/btc', function (req, response, next) {
-
-    const blockchain$ = observableFromFeed(blockchainFeed)
-    const coindesk$ = observableFromFeed(coindeskFeed)
-    const bitcoinchart$ = observableFromFeed(bitcoinchartsFeed)
-
-    const feeds = [blockchain$, coindesk$, bitcoinchart$]
-
-    Observable.merge(...feeds)
-        .bufferCount(feeds.length)
-        .subscribe(
-            data => {
-                const maxPrice = packageTheResult(data)
-                response.json({
-                    'winner': maxPrice,
-                    'raw': data,
-                    'sources': feeds.length
-                })
-            }, ({code}) => {
-                response.status(500).send(code)
-            })
-
+    res.status(200).send('ticker stopped, no more emits')
 })
 
 export {router}
